4de36d9f0a691b4fff99ebb18dbf79d0f2ef7b1f
iverissimo <verissimo.iness@gmail.com>
2025-01-26 20:42:40 +0100

    ordering timeseries plot by month name (not ordinal)

--- scripts/visualize.R
+++ scripts/visualize.R
@@ -8,15 +8,26 @@ plot_ndvi_timeseries <- function(train_data = NULL, test_data = NULL,
                                  country_name = NULL, resolution = NULL,
                                  plot_width = 15, plot_height = 8,
                                  ylim_range = c(0.15, 0.75),
+                                 test_start_date = NULL, test_end_date = NULL,
                                  label_test = "NDVI 2024",
                                  label_train = "NDVI 2019-2023",
-                                 save_path = NULL, filename = "NDVI_timeseries.png") {
+                                 save_path = NULL,
+                                 filename = "NDVI_timeseries.png") {
+
+  # Add Month Name to dataframes
+  test_data$Month_Name <- month.name[as.numeric(test_data$Month)]
+  train_data$Month_Name <- month.name[as.numeric(train_data$Month)]
+
+  # Make month name vector, to customize order of x axis 
+  month_vector <- format(seq(test_start_date, test_end_date, by="month"), "%B")
+
   # Set plot size
   options(repr.plot.width = plot_width, repr.plot.height = plot_height)
 
   # Create the plot
-  ts_plot <- ggplot(NULL, aes(x = Month, y = mean_val, group = 1)) +
-    geom_point(data = train_data, size = 5, 
+  ts_plot <- ggplot(NULL, aes(x = factor(Month_Name, level=month_vector),
+                              y = mean_val, group = 1)) +
+    geom_point(data = train_data, size = 5,
                fill = "#2781cf") + # point-average train data
     geom_line(data = train_data) +
     geom_ribbon(data = train_data, aes(ymin = lower_ci, ymax = upper_ci),
@@ -37,9 +48,9 @@ plot_ndvi_timeseries <- function(train_data = NULL, test_data = NULL,
       plot.title = element_text(size = 20, hjust = 0.5)
     ) +
     ylim(ylim_range) +
-    geom_text(x = 10, y = ylim_range[2] - 0.05, label = label_test, size = 8,
+    geom_text(x = 10, y = ylim_range[2] - 0.05, label = label_test, size = 6,
               color = "#9662b3", hjust = 0) + # add text to label plot
-    geom_text(x = 10, y = ylim_range[2] - 0.1, label = label_train, size = 8,
+    geom_text(x = 10, y = ylim_range[2] - 0.1, label = label_train, size = 6,
               color = "#2781cf", hjust = 0) # add text to label plot
 
   # Save plot if save_path is provided
